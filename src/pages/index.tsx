142e37d0643bf3adcba9f69278c05f613f8eb9a5
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-04 23:36:13 -0300

    preparando arquivos iniciais

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export const Home: NextPage = () => {
+	return (
+		<main className=''>
+
+		</main>
+	)
+}
