9568112d6aee2c6ce212d11a281e666161304834
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-06 13:10:07 -0300

    Testando component List

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import initialTasks from '../data/mock'
 import Task from '../model/Task'
-import ListItem from '../components/list/ListItem'
+import List from '../components/list/List'
 // import Selection from '../components/list/Selection'
 //import Task from '../model/Task'
 
@@ -16,15 +16,15 @@ type TaskProps = {
 
 export default function Home() {
 
-	// const [tasks, setTasks] = useState<any>([])
+	const [tasks, setTasks] = useState(initialTasks)
 
 	// useEffect(() => {
 	// 	setTasks(initialTasks.allTasks)
 	// 	console.log(tasks)
-	// }, [])
+	// // }, [])
 
-	let tasks = initialTasks
-	tasks = tasks.addTask(Task.createActiveTask(4, 'Program TCC'))
+	// let tasks = initialTasks
+	// tasks = tasks.addTask(Task.createActiveTask(4, 'Program TCC'))
 
 	function renderTasks(){
 		return tasks.allTasks.map((task: TaskProps) => {
@@ -39,11 +39,7 @@ export default function Home() {
 
 	return (
 		<main className='flex flex-col justify-center items-center h-screen text-white bg-gradient-to-tr from-purple-500 to-yellow-600'>
-			{renderTasks()}
-			<ul>
-				<ListItem taskDescription='List Item 1' finished alternateStatus={() => {}}/>
-				<ListItem taskDescription='List Item 2' finished={false} alternateStatus={() => {}}/>
-			</ul>
+			<List tasks={tasks} changed={newTasks => {setTasks(newTasks)}} />
 		</main>
 	)
 }
