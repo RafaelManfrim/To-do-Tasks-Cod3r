dd3ca3b9739723fd97dcedd24f605547e44fac94
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-07 21:59:27 -0300

    Removendo as tasks iniciais

--- src/data/mock.ts
+++ src/data/mock.ts
@@ -3,9 +3,9 @@ import Task from '../model/Task'
 import TaskList from '../model/TaskList'
 
 const initialTasks: Task[] = [
-    Task.createActiveTask(1, 'Study Next'),
-    Task.createFinishedTask(2, 'Clean the bedroom'),
-    Task.createActiveTask(3, 'Fix the car'),
+    // Task.createActiveTask(1, 'Study Next'),
+    // Task.createFinishedTask(2, 'Clean the bedroom'),
+    // Task.createActiveTask(3, 'Fix the car'),
 ]
 
 export default new TaskList(initialTasks, FilterType.NONE)
